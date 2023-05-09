cd8bdb10cfeb0854617927eed7a19bd9782c0277
Cable <cableplays1912@gmail.com>
2023-05-09 18:32:27 +0200

    Update TODO

--- dev/sql.sql
+++ dev/sql.sql
@@ -1,1 +1,3 @@
-SELECT * FROM rock_climbing_subrecords;
+-- INSERT INTO users VALUES (3, "test@gmail.com", "abc", "Astra Spero", "Astra", "Spero");
+UPDATE users SET name = "John Doe", given_name = "John", family_name = "Doe" WHERE id = 2;
+SELECT * FROM users;

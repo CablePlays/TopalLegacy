09a1d809ac23598ec9e86b14e7664c0a614a6b97
Cable <cableplays1912@gmail.com>
2023-05-27 12:05:16 +0200

    Update Milestones Page

--- public/javascripts/profile/milestones.js
+++ public/javascripts/profile/milestones.js
@@ -1,31 +1,29 @@
 function loadMilestones() {
     const milestonesTable = document.getElementById("milestones-table");
     const template = [
-        ["team", "Team Award"],
-        ["halfColors", "Half Colors"],
-        ["colors", "Colors"],
-        ["merit", "Merit Award"],
-        ["honors", "Honors"]
+        ["team", "Team Award", "Requires 4 awards"],
+        ["halfColors", "Half Colors", "Requires 7 awards"],
+        ["colors", "Colors", "Requires 10 awards"],
+        ["merit", "Merit Award", "Awarded, not achieved"],
+        ["honors", "Honors", "Requirements coming soon"]
     ];
 
     const milestonesPromise = new Promise(async r => r((await getRequest(`/users/${getProfileUser()}/milestones`)).milestones));
 
     template.forEach(a => {
-        const id = a[0];
-        const display = a[1];
+        const [id, title, description] = a;
 
         const tr = document.createElement("tr");
+        milestonesTable.append(tr);
 
-        const title = document.createElement("td");
-        title.innerHTML = display;
-        tr.appendChild(title);
+        const textContainer = createElement("td", tr);
+        createElement("h2", textContainer, title).classList.add("title");
+        createElement("p", textContainer, description).classList.add("description");
 
         const checkbox = document.createElement("td");
         const checkboxImage = createCheckbox(new Promise(async r => r((await milestonesPromise)[id] === true)));
         checkbox.appendChild(checkboxImage);
         tr.appendChild(checkbox);
-
-        milestonesTable.append(tr);
     });
 }
 
--- requests/users/milestones.js
+++ requests/users/milestones.js
@@ -6,10 +6,10 @@ const router = express.Router();
 router.get("/", (req, res) => {
     const targetUserId = req.userId;
     const awards = jsonDatabase.getUser(targetUserId).get(jsonDatabase.AWARDS_PATH) ?? {};
-    const totalAwards = 0;
+    let totalAwards = 0;
 
     for (let award of Object.getOwnPropertyNames(awards)) {
-        if (award.complete === true) {
+        if (awards[award].complete === true) {
             totalAwards++;
         }
     }

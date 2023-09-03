613c6312cbc0b09df377e5ae85b27284ddce53f7
Cable <cableplays1912@gmail.com>
2023-09-03 15:17:02 +0200

    Minor Updates

--- render/awards-router.js
+++ render/awards-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const middleware = require("./middleware");;
+const middleware = require("./middleware");
 
 const router = express.Router();
 
